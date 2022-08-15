b538b3f91287e899b26748d03984cd38d6fe4b94
Dhandeshwaran K S <58393199+dhandesh1992@users.noreply.github.com>
2022-08-16 01:13:48 +0530

    Create lottery_commission_updated.sol
    It's the lottery.sol script with some improvemnts :
    
    1) The purchase of lotteries can be done by choosing the number of tickets, you want to purchase and keep the value as ether and click the button "Buy" function on deployment.
    2) No Reentrancy modifier is used to stop reentrancy hack.
    3) The commission is received by  manager too, when the manager withdraws the winnings... 
    4) No one else can withdraw the amount other than the manager, as it will be easy to choose the winner as well as send the payment.

---
+++ lottery_commission_updated.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Lottery{
+
+    address payable public manager;
+    bool private locked;
+    address payable[] public participants;
+    uint public ticketCount;
+    address payable winner;
+    mapping (address => uint) balances;
+
+    struct Manager {
+        uint rand;
+    }
+
+    Manager oracle;
+
+    constructor() {
+        uint i = participants.length;
+        manager = payable(msg.sender);
+        oracle = Manager(i);
+    }
+
+    modifier onlyBy() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    modifier nonReentrant() {
+    require(!locked, "No Reentrancy");
+    locked = true;
+    _;
+    locked = false;
+    }
+
+    function Buy() payable external {
+        require (msg.value >= 1 ether && msg.value % 1 ether == 0, "No decimals allowed");
+        uint val = msg.value / 1 ether;
+        for (uint i = 0; i < val; i++){
+            participants.push(payable(msg.sender));
+        }
+        ticketCount += val;
+    }
+
+    function getBalance() public view returns(uint) {
+        require (msg.sender == manager);
+        return address(this).balance;
+    }
+
+    function withdrawManagerFunds() private {
+        manager.transfer(getBalance());
+        ticketCount = 0;
+        participants = new address payable[](0);
+    }
+
+    function getWinner() public onlyBy() returns (uint rand){
+        require(participants.length >= 3);
+        rand = uint (keccak256(abi.encodePacked(block.timestamp,msg.sender))) % participants.length;
+        winner = participants[rand];
+    }
+
+    function lastWinner() public view returns(address){
+        return winner;
+    }
+
+    function withdraw() public nonReentrant() {
+        winner.transfer(getBalance()*9/10);
+        withdrawManagerFunds();
+    }
+}
